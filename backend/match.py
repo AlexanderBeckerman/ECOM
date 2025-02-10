0399141c595c701b9756c26ac78e9051ed8a1651
rotemzahuri@mail.tau.ac.il <rotemzahuri@mail.tau.ac.il>
2025-02-10 05:58:08 +0200

    match restaurants to a customer

--- backend/match.py
+++ backend/match.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-def euclidean_distance(customer_scores, restaurant_scores):
-    return np.linalg.norm(np.array(customer_scores) - np.array(restaurant_scores))
+#def euclidean_distance(customer_scores, restaurant_scores):
+#    return np.linalg.norm(np.array(customer_scores) - np.array(restaurant_scores))
 
 def calculate_weights(customer_scores):
     # Higher weight for scores near the edges (1 or 5), lower weight for scores near the middle (3)
@@ -11,21 +11,23 @@ def weighted_score(customer_scores, restaurant_scores, weights):
     return sum(w * (5 - abs(r - c)) for w, r, c in zip(weights, restaurant_scores, customer_scores))
 
 
-def rank_restaurants(customer, restaurants, categories, max_threshold=3, min_threshold=0.5):
+def rank_restaurants(customer_scores, restaurants, categories, max_threshold=3, min_threshold=0.3):
     rankings = []
     
-    for restaurant_id, restaurant_scores in restaurants.items():
-        differences = np.abs(np.array(customer) - np.array(restaurant_scores))
+    weights = calculate_weights(customer_scores) # option 2
+    for restaurant_id, restaurant_data in restaurants.items():
+        restaurant_scores = [restaurant_data.get(category, 3) for category in categories] #set default score to 3 if category not found
         
-        if np.any(differences > max_threshold):  # If any category difference is over the threshold: skip
+        differences = np.abs(np.array(customer_scores) - np.array(restaurant_scores))
+        
+        if np.any(differences > max_threshold):  # If any category difference is over the threshold: skip restaurant
             continue
         
-        similar_categories = [categories[i] for i in range(len(differences)) if differences[i] <= min_threshold]
+        similar_categories = [categories[i] for i in range(len(differences)) if differences[i] <= min_threshold] # mark categories that are similar
 
-        score = euclidean_distance(customer, restaurant_scores) # option 1
+        #score = euclidean_distance(customer, restaurant_scores) # option 1
         
-        #weights = calculate_weights(customer) ######################## option 2
-        #score = weighted_score(customer, restaurant_scores, weights) # option 2
+        score = weighted_score(customer_scores, restaurant_scores, weights) # option 2
 
         rankings.append((restaurant_id, score, similar_categories))
     
