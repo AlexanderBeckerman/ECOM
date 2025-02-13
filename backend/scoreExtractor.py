825a4b6f69e60d57f821e16df84ea7872c98898a
beckermana <beckermana@mail.tau.ac.il>
2025-02-13 22:52:57 +0200

    trying out ABSA (aspect based sentiment analysis) model

--- backend/scoreExtractor.py
+++ backend/scoreExtractor.py
@@ -7,10 +7,12 @@ from base import Session
 from models.models import Review, Restaurant
 from sqlalchemy import func
 from backend.helpers.sentence_splitter import split_into_sentences
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 
 # from reliability_calculator import calculate_review_reliability
 
 business_to_scores = {}
+categories = ["food", "price", "service", "ambience"]
 
 
 def filter_reviews(reviews):
@@ -38,7 +40,7 @@ def get_reviews_for_business(business_id):
 
 
 # Function to extract category ratings from reviews
-def extract_category_ratings(reviews, categories, relevance_classifier, sentiment_analyzer):
+def extract_category_ratings(reviews, relevance_classifier, sentiment_analyzer):
     category_scores = defaultdict(list)
     n = len(reviews)
     personal_category_scores = [{} for i in range(n)]
@@ -72,24 +74,60 @@ def extract_category_ratings(reviews, categories, relevance_classifier, sentimen
     result = {}
     for category, scores in category_scores.items():
         if scores:
-            result[category] = np.dot(scores, category_relevance_scores[category]) / sum(
-                category_relevance_scores[category])
+            result[category] = np.around(np.dot(scores, category_relevance_scores[category]) / sum(
+                category_relevance_scores[category]), decimals=2)
         else:
             result[category] = 0
 
     return result, personal_category_scores
 
 
-def get_scores_for_all_businesses(relevance_classifier, sentiment_analyzer):
+def extract_category_ratings_using_absa(reviews, absa_pipeline):
+    sentiment_map = {"Positive": 5, "Neutral": 3, "Negative": 1}
+    category_scores = {cat: [] for cat in categories}
+    category_confidences = {cat: [] for cat in categories}
+    personal_category_scores = [{} for i in range(len(reviews))]
+    cnt = 0
+    for review in reviews:
+        for category in categories:
+            result = absa_pipeline(review, text_pair=category)
+            predicted_sentiment = result[0]
+            print(f"Predicted Sentiment : {predicted_sentiment}")
+            sentiment_label = predicted_sentiment['label']
+            confidence = predicted_sentiment['score']
+            sentiment = sentiment_map[sentiment_label]
+            print(f"sentiment: {sentiment_label}, confidence: {confidence}, category: {category}, review:\n {review}")
+            if confidence > 0.75:
+                if sentiment_label == "Positive":
+                    curr_score = 3 + 2 * confidence
+                elif sentiment_label == "Negative":
+                    curr_score = 3 - 2 * confidence
+                else:
+                    curr_score = 0
+                category_scores[category].append(curr_score)
+                category_confidences[category].append(confidence)
+                personal_category_scores[cnt][category] = sentiment
+
+        cnt += 1
+    final_scores = {
+        cat: round(sum(scores) / len(scores), 2) if scores else 0
+        for cat, scores in category_scores.items()
+    }
+    return final_scores, personal_category_scores
+
+
+def get_scores_for_all_businesses(relevance_classifier=None, sentiment_analyzer=None, absa_pipeline=None):
     with Session() as session:
         business_ids = session.query(Review.business_id).distinct().all()
         business_ids = [id[0] for id in business_ids]
         executor = ThreadPoolExecutor(max_workers=4)
         results = executor.map(get_reviews_for_business, business_ids)
         for business_id, reviews in zip(business_ids, results):
-            categories = ["food", "service", "music", "price"]
-            result, personal_category_scores = extract_category_ratings(reviews, categories, relevance_classifier,
-                                                                        sentiment_analyzer)
+            if relevance_classifier and sentiment_analyzer:
+                result, personal_category_scores = extract_category_ratings(reviews, relevance_classifier,
+                                                                            sentiment_analyzer)
+            elif absa_pipeline:
+                result, personal_category_scores = extract_category_ratings_using_absa(reviews, absa_pipeline)
             business_to_scores[business_id] = result
             print(f"Business ID: {business_id}, Category Scores: {result}")
             print()
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from loaders.load_reviews import load_reviews, load_reviews_by_business
 from loaders.load_restaurants import load_restaurants
 from models.models import User, Restaurant, Review
 from sqlalchemy import inspect
-from transformers import pipeline
+from transformers import pipeline, DebertaV2Tokenizer, AutoModelForSequenceClassification
 from backend.scoreExtractor import get_scores_for_all_businesses
 from backend.helpers.sentence_splitter import split_into_sentences
 from flask import Flask, jsonify
@@ -25,25 +25,30 @@ def main():
     create_tables()  # Create tables if they don't exist
 
     # Uncomment if you want to load data from scratch
-    with Session() as session:
-        load_users(session, USERS_FILEPATH)
-        load_restaurants(session, RESTAURANTS_FILEPATH, limit=15)
-        business_id = session.query(Restaurant.business_id).all()
-        for id in business_id:
-            business_id = id[0]
-            review_cnt = session.query(Restaurant.review_count).filter(Restaurant.business_id == business_id).first()[0]
-            try:
-                load_reviews_by_business(session, REVIEWS_FILEPATH, business_id, review_cnt)
-            except sqlite3.InternalError as e:
-                continue
+    # with Session() as session:
+    #     load_users(session, USERS_FILEPATH)
+    #     load_restaurants(session, RESTAURANTS_FILEPATH, limit=7)
+    #     business_id = session.query(Restaurant.business_id).all()
+    #     for id in business_id:
+    #         business_id = id[0]
+    #         review_cnt = session.query(Restaurant.review_count).filter(Restaurant.business_id == business_id).first()[0]
+    #         try:
+    #             load_reviews_by_business(session, REVIEWS_FILEPATH, business_id, review_cnt)
+    #         except sqlite3.InternalError as e:
+    #             continue
 
     # Load NLP models
-    relevance_classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-    sentiment_analyzer = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
-
+    # relevance_classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+    # sentiment_analyzer = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+    model_name = "yangheng/deberta-v3-base-absa-v1.1"
+    tokenizer = DebertaV2Tokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    classifier = pipeline("text-classification", model=model, tokenizer=tokenizer)
+    # for aspect in ['camera', 'phone']:
+    #     print(aspect, classifier('The camera quality of this phone is amazing.', text_pair=aspect))
     # Uncomment if you want to calculate scores for all businesses in advance
-    get_scores_for_all_businesses(relevance_classifier,
-                                  sentiment_analyzer)  # Calculate scores for all businesses in advance
+    get_scores_for_all_businesses(relevance_classifier=None, sentiment_analyzer=None,
+                                  absa_pipeline=classifier)  # Calculate scores for all businesses in advance
 
 
 if __name__ == '__main__':
